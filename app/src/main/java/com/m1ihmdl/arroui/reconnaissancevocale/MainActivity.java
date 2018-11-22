a235e54a38f019f8f1832b0c76c43510247e502c
Sid Ahmed <sidahmedarroui@hotmail.com>
2018-11-22 11:05:27 +0100

    Enregistrements des voix dans le dossier cache externe accesible par la méthode getExternalCacheDir().getAbsolutePath()dans l'interface on peut consulter les ordres qu'on as déjà enregistrer.
    Signed-off-by: Sid Ahmed <sidahmedarroui@hotmail.com>

--- app/src/main/java/com/m1ihmdl/arroui/reconnaissancevocale/MainActivity.java
+++ app/src/main/java/com/m1ihmdl/arroui/reconnaissancevocale/MainActivity.java
@@ -1,42 +1,49 @@
 package com.m1ihmdl.arroui.reconnaissancevocale;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String LOG_TAG = "AudioPlayTest";
+    private List<String> fileList = new ArrayList<>();
+    private MediaPlayer mPlayer;
+    private String basePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        basePath = getExternalCacheDir().getAbsolutePath();
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        Button settings = (Button) findViewById(R.id.action_settings);
-        settings.setOnClickListener(new View.OnClickListener() {
+        final File base = new File(basePath);
+        Button checkFiles = findViewById(R.id.checkFiles);
+        checkFiles.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, Settings.class));
+                listDir(base);
             }
         });
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+
     }
 
     @Override
@@ -55,11 +62,47 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this, RecordingActivity.class));
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
+    private void startPlaying(String mFileName) {
+        mPlayer = new MediaPlayer();
+        try {
+            mPlayer.setDataSource(basePath + mFileName);
+            mPlayer.prepare();
+            mPlayer.start();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "prepare() failed");
+        }
+    }
+
+    private void stopPlaying() {
+        mPlayer.release();
+        mPlayer = null;
+    }
+
+    void listDir(File f) {
+        ListView listView = findViewById(R.id.listView);
+        File[] files = f.listFiles();
+        fileList.clear();
+        for (File file : files) {
+            fileList.add(file.getName());
+        }
+        ArrayAdapter<String> directoryList = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, fileList);
+        listView.setAdapter(directoryList);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mPlayer != null) {
+            mPlayer.release();
+            mPlayer = null;
+        }
+    }
 
 }
---
+++ app/src/main/java/com/m1ihmdl/arroui/reconnaissancevocale/RecordingActivity.java
@@ -0,0 +1,206 @@
+package com.m1ihmdl.arroui.reconnaissancevocale;
+
+import android.Manifest;
+import android.app.Dialog;
+import android.content.pm.PackageManager;
+import android.media.MediaRecorder;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+public class RecordingActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = "AudioRecordTest";
+    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
+    private MediaRecorder mRecorder;
+    private String basePath;
+    private Dialog myDialog;
+    private boolean permissionToRecordAccepted = false;
+    private boolean recording = false;
+    private String[] permissions = {Manifest.permission.RECORD_AUDIO};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+
+        ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION);
+        basePath = getExternalCacheDir().getAbsolutePath();
+        myDialog = new Dialog(this);
+        myDialog.setContentView(R.layout.custompopup);
+
+        Button avance = findViewById(R.id.avance);
+        Button recule = findViewById(R.id.recule);
+        Button droite = findViewById(R.id.droite);
+        Button gauche = findViewById(R.id.gauche);
+        Button tourneDroite = findViewById(R.id.tourneDroite);
+        Button tourneGauche = findViewById(R.id.tourneGauche);
+        Button flip = findViewById(R.id.flip);
+        Button etatUrgence = findViewById(R.id.etatUrgence);
+        Button arreteToi = findViewById(R.id.arreteToi);
+
+        Button cancelpopupButton = myDialog.findViewById(R.id.cancelpopup);
+        cancelpopupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopRecording();
+                recording = false;
+                myDialog.dismiss();
+            }
+        });
+        Button stopRecordingButton =
+                myDialog.findViewById(R.id.stopRecording);
+        stopRecordingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopRecording();
+                recording = false;
+                myDialog.dismiss();
+            }
+        });
+
+        avance.setOnClickListener(new View.OnClickListener() {
+            String fileOutPutName = "avance";
+
+            public void onClick(View v) {
+                TextView text = myDialog.findViewById(R.id.ordre);
+                text.setText("Veuillez prononcer le mot avance");
+                startRecording(fileOutPutName);
+                myDialog.show();
+            }
+        });
+        recule.setOnClickListener(new View.OnClickListener() {
+            String fileOutPutName = "recule";
+
+            public void onClick(View v) {
+                ((TextView) myDialog.findViewById(R.id.ordre)).setText("Veuillez prononcer le mot recule");
+                startRecording(fileOutPutName);
+                myDialog.show();
+            }
+        });
+        droite.setOnClickListener(new View.OnClickListener() {
+            String fileOutPutName = "droite";
+
+            public void onClick(View v) {
+                ((TextView) myDialog.findViewById(R.id.ordre)).setText("Veuillez prononcer le mot droite");
+                startRecording(fileOutPutName);
+                myDialog.show();
+            }
+        });
+        gauche.setOnClickListener(new View.OnClickListener() {
+            String fileOutPutName = "gauche";
+
+            public void onClick(View v) {
+                ((TextView) myDialog.findViewById(R.id.ordre)).setText("Veuillez prononcer le mot gauche");
+                startRecording(fileOutPutName);
+                myDialog.show();
+            }
+        });
+        tourneGauche.setOnClickListener(new View.OnClickListener() {
+
+            String fileOutPutName = "tourneGauche";
+
+            public void onClick(View v) {
+                ((TextView) myDialog.findViewById(R.id.ordre)).setText("Veuillez prononcer le mot tourneGauche");
+                startRecording(fileOutPutName);
+                myDialog.show();
+            }
+        });
+        tourneDroite.setOnClickListener(new View.OnClickListener() {
+            String fileOutPutName = "tourneDroite";
+
+            public void onClick(View v) {
+                ((TextView) myDialog.findViewById(R.id.ordre)).setText("Veuillez prononcer le mot tourneDroite");
+                startRecording(fileOutPutName);
+                myDialog.show();
+            }
+        });
+        flip.setOnClickListener(new View.OnClickListener() {
+            String fileOutPutName = "flip";
+
+            public void onClick(View v) {
+                ((TextView) myDialog.findViewById(R.id.ordre)).setText("Veuillez prononcer le mot flip");
+                startRecording(fileOutPutName);
+                myDialog.show();
+            }
+        });
+        etatUrgence.setOnClickListener(new View.OnClickListener() {
+            String fileOutPutName = "etatUrgence";
+
+            public void onClick(View v) {
+                ((TextView) myDialog.findViewById(R.id.ordre)).setText("Veuillez prononcer le mot etatUrgence");
+                startRecording(fileOutPutName);
+                myDialog.show();
+            }
+        });
+        arreteToi.setOnClickListener(new View.OnClickListener() {
+            String fileOutPutName = "arreteToi";
+
+            public void onClick(View v) {
+                ((TextView) myDialog.findViewById(R.id.ordre)).setText("Veuillez prononcer le mot arreteToi");
+                startRecording(fileOutPutName);
+                myDialog.show();
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_RECORD_AUDIO_PERMISSION:
+                permissionToRecordAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                break;
+        }
+        if (!permissionToRecordAccepted) finish();
+
+    }
+
+
+    private void startRecording(String mFileName) {
+        if (!recording) {
+            mRecorder = new MediaRecorder();
+            mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            mRecorder.setOutputFile(basePath + "/" + mFileName);
+            mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+            try {
+                mRecorder.prepare();
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "prepare() failed");
+            }
+
+            mRecorder.start();
+            recording = true;
+        }
+
+    }
+
+    private void stopRecording() {
+        if (recording) {
+            mRecorder.stop();
+            mRecorder.release();
+            mRecorder = null;
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mRecorder != null) {
+            mRecorder.release();
+            mRecorder = null;
+        }
+    }
+
+
+}
